4f00cf56c266031adefdc71949e0954e73280cc9
eedom <70550201+eedom@users.noreply.github.com>
2022-02-14 12:47:09 -0700

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -7,9 +7,7 @@ def bubbleSort(array):
     #check if it is sorted
     isSorted = False
     counter = 0
-    #while the array id not sorted 
-    # we minus the counter is because after we finish our loop we are going to tentatively go back into while loop,We increment out counter
-    #which is gonna say that in the next for loop we are gonna only go to one position befor the position that we went to 
+   
     while not isSorted:
         isSorted = True
         for i in range(len(array) -1 - counter):  
